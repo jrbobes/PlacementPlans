fe7d16c9a764c777b4949d1fc94aeb7f879a1256
Bobes <jrbobes@gmail.com>
2017-11-03 02:41:51 +0100

    Actualización en formulario de login

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -2,14 +2,18 @@ package sample;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
 
 public class Controller {
 
     @FXML private Text actiontarget;
+    @FXML private TextField userNameField;
+    @FXML private PasswordField passwordField;
 
     @FXML
     protected void handleSubmitButtonAction(ActionEvent event) {
-        actiontarget.setText("Botonazo!");
+        actiontarget.setText("- " + userNameField.getText() + " / " + passwordField.getText() + " -");
     }
 }
