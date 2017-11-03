136763bcf245206abe202e9380cd9a7d4d0be0e2
Bobes <jrbobes@gmail.com>
2017-11-03 02:29:17 +0100

    Initial commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,15 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.text.Text;
+
+public class Controller {
+
+    @FXML private Text actiontarget;
+
+    @FXML
+    protected void handleSubmitButtonAction(ActionEvent event) {
+        actiontarget.setText("Botonazo!");
+    }
+}
